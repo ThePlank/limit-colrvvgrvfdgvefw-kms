ec3bb857898fd9dff4ba78dd98f4a652be56d4c1
ThePlank <75984453+ThePlank@users.noreply.github.com>
2023-08-09 15:56:47 +0200

    bop

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -227,7 +227,7 @@ class TitleState extends MusicBeatState {
 		add(bg);
 	
 		logoBl = new FlxSprite(700, 75).loadGraphic(Paths.image('logoBump'));
-		logoBl.scale.set(3, 3);
+		logoBl.scale.set(2, 2);
 		logoBl.antialiasing = false;
 		logoBl.updateHitbox();
 		logoBl.cameras = [camOther];
@@ -412,10 +412,11 @@ class TitleState extends MusicBeatState {
 	public static var closedState:Bool = false;
 	override function beatHit() {
 		super.beatHit();
-		
-		//ok i will not
-		// FlxTween.tween(logoBl.scale, {y: logoBl.scale.y - 1, x: logoBl.scale.x - 1}, 1, {ease: FlxEase.cubeInOut, type: LOOPING});
 
+		FlxTween.cancelTweensOf(logoBl);
+		logoBl.scale.set(2.2, 2.2);
+		FlxTween.tween(logoBl.scale, {x: 2, y: 2}, (Conductor.crochet / 1000) * 0.5, {ease: FlxEase.expoOut}); // fyu the duration of this tween is 2 steps :3
+		
 		if(!closedState) {
 			sickBeats++;
 			switch (sickBeats) {
