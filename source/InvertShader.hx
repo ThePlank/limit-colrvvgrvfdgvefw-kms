4257f39404df3af7ea8e76f2a86e4be9e7ec7d65
ThePlank <75984453+ThePlank@users.noreply.github.com>
2023-08-15 17:07:51 +0200

    die

--- source/InvertShader.hx
+++ source/InvertShader.hx
@@ -7,7 +7,8 @@ class InvertShader extends FlxShader {
 		#pragma header
 		void main() {
 		    vec4 tex = flixel_texture2D(bitmap, openfl_TextureCoordv.xy);
-		    gl_FragColor = vec4((1.0 - tex.r) * tex.a, (1.0 - tex.g) * tex.a, (1.0 - tex.b) * tex.a, tex.a);
+		    tex.rgb = tex.a * (1.0 - tex.rgb);
+		    gl_FragColor = tex;
 		}
 	')
 
